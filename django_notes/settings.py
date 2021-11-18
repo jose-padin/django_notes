da5d8c33790b93de4d382e97d03eaf3cc21c3705
jose.padin <jose.mendez@kubicum.com>
2021-11-18 20:50:52 +0100

    Fixed issue

--- django_notes/settings.py
+++ django_notes/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03cpr(=1-u-_x3p7sdl1p*901-jmo-y+9$9b_fb=p8%^16np6o'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "secret_key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
